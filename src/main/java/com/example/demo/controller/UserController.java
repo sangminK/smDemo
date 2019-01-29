55b8d2c2a36f0aa5ac0d0631aec2aa8fd2cc3896
김상민 <smkim@gaeasoft.co.kr>
2019-01-29 11:33:48 +0900

    User CRUD 완료

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.demo.domain.User;
 import com.example.demo.service.UserService;
@@ -18,21 +20,47 @@ import com.example.demo.service.UserService;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+	
     @Autowired
     UserService userService;
-
-    @RequestMapping(value = "/all", method = RequestMethod.GET)
-    public List<User> getAllUsers() {
-        return userService.getAllUsers();
-    }
-
-    @RequestMapping(value = "/adduser", method = RequestMethod.POST,
+    
+    // create 생성(insert)
+    @RequestMapping(value = "/add", method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody()
     public User addNewUser(@RequestBody User user) {
         return this.userService.addUser(user);
     }
-    
-    //other controllers omitted for brevity
 
+    // read 조회
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+    
+    // ID로 조회
+    @RequestMapping(value = "/getone/{id}", method = RequestMethod.GET)
+    public Optional<User> getById(@PathVariable int id) {
+    	return userService.getUser(id);
+    }
+    
+    // update 수정 - name 수정
+    @RequestMapping(value = "/update-name/{id}/{name}", method = RequestMethod.POST)
+    public User updateUserName(@PathVariable int id, @PathVariable String name) {
+    	return this.userService.updateName(id, name);
+    }
+    
+    // update 수정 - salary 수정
+    @RequestMapping(value = "/update-salary/{id}/{salary}", method = RequestMethod.POST)
+    public User updateUserSalary(@PathVariable int id, @PathVariable int salary) {
+    	return this.userService.updateSalary(id, salary);
+    }
+    
+    // delete 삭제
+    @RequestMapping(value = "/delete/{id}")
+    public String deleteById(@PathVariable int id) {
+    	userService.deleteUser(id);
+    	return "delete complete";
+    }
+    
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,16 +11,42 @@ import com.example.demo.dto.UserDao;
 
 @Service
 public class UserService {
+	
     @Autowired
     UserDao userDao;
 
+    // 전체 조회
     public List<User> getAllUsers() {
         return this.userDao.findAll();
     }
+    
+    // ID로 조회
+    public Optional<User> getUser(int id) {
+    	return this.userDao.findById(id);
+    }
 
+    // 데이터 추가
     public User addUser(User user) {
         return this.userDao.save(user);
     }
 
-    //other methods omitted for brevity
+    // 수정
+    public User updateName(int id, String name) {
+    	User user = userDao.getOne(id);
+    	user.setName(name);
+    	return this.userDao.save(user);
+    }
+    
+    // 수정
+    public User updateSalary(int id, int salary) {
+    	User user = userDao.getOne(id);
+    	user.setSalary(salary);
+    	return this.userDao.save(user);
+    }
+    
+    // 삭제
+    public void deleteUser(int id) {
+    	this.userDao.deleteById(id);
+    }
+
 }
